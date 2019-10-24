ae541a0424931e35449321bf0473378e5d290af5
Damian Clitus <daniel.robertamalraj@hotmail.com>
2019-10-25 00:30:40 +0200

    v3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,17 @@
-alert("summa")
+//Cheks the li
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function () {
+    $(this).parent().remove();
+});
+
+$("input[type='text']").keypress(function (e) {
+    if (e.which === 13) {
+        var todo = $(this).val();
+        $(this).val("");
+
+        $("ul").append("<li><span>x</span> " + todo + "</li>");
+    }
+});
